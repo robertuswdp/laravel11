9acb74c0a984baf3dace7a878040dfbc92fcac1e
robertuswdp <robertuswdp@gmail.com>
2024-06-10 10:24:23 +0700

    4. Blade Templating Engine

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <i class="fa fa-instagram" aria-hidden="true"><a href="instagram.com/robertuswanda"></a></i>
+        </li>
+        <li>
+            <a href="instagram.com/robertuswanda"></a>
+        </li>
+        <li>
+            <a href="instagram.com/robertuswanda"></a>
+        </li>
+    </ul>
+</body>
+
+</html>
