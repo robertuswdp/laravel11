d96dac7e1fbc7ab5cc95ad77c73f2cb051b5e712
robertuswdp <robertuswdp@gmail.com>
2024-06-12 18:31:07 +0700

    5. Blade Component

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title'=>'Home']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama'=>'robertuswanda']);
+    return view('about', [
+        'title'=>'About',
+        'name'=>'robertuswanda'
+    ]);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title'=>'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title'=>'Contact']);
 });
